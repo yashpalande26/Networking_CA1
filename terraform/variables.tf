63bde9b896f2f24594b32eaf91576f6fa2a67e26
yashpalande26 <125365662+yashpalande26@users.noreply.github.com>
2024-11-09 23:19:27 +0000

    Terraform - outputs,variables

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "Amazon Linux 2 AMI ID"
+  type        = string
+  default     = "ami-03ca36368dbc9cfa1"  # Amazon Linux 2 AMI in us-west-2
+}
