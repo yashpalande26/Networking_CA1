6ce8ddc16cb7d60d8f1a62dda94ffbd4b7b0fe5a
Yash Palande <20043004@mydbs.ie>
2024-11-10 01:56:39 +0000

    test8

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to our Website"
+    return "Welcome to our Website !!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
